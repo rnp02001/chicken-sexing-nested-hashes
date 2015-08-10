082fc48bc90718dfb2cc3593d5adb8fab3256b16
Ryan Ponte <ryan.ponte@gmail.com>
2015-08-10 09:19:23 -0400

    fix typos

--- methods.rb
+++ methods.rb
@@ -43,24 +43,3 @@ def title_or_breed(member)
   found_member = find_member_by_name(member)
   found_member[:title_or_breed] if found_member
 end
-
-# def team_names
-#   [home_team[:team_name], away_team[:team_name]]
-# end
-#
-# def player_numbers(team)
-#   find_team_by_name(team)[:players].map { |p| p[:number] }
-# end
-#
-# def player_stats(player)
-#   found_player = find_player_by_name(player)
-#   found_player.delete(:player_name)
-#   found_player
-# end
-#
-# def big_shoe_rebounds
-#   all_players.sort_by {|p| -p[:shoe]}.first[:rebounds]
-# end
-
-p find_species_by_name(chickens)
-p find_members_by_pecking_order(8)
